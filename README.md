41268537cbc790561112d5db1d1bb6dfedd2e839
Gabriele Fontana <gafreax@gmail.com>
2023-02-01 03:38:03 +0100

    first release

---
+++ README.md
@@ -0,0 +1,10 @@
+# turn-the-light README
+
+Simple extension to turn light theme by clicking on status bar and allow the user to switchback with a second click
+
+
+### 0.1.0
+
+Initial release of Turn the light
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,48 @@
+const vscode = require("vscode");
+let defaultTheme = vscode.workspace.getConfiguration().get('workbench.colorTheme');
+
+// Activate the extension
+function activate(context) {
+    // Create the toggle button
+    let toggleButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10000000);
+    toggleButton.command = 'turn-the-light.switch';
+    toggleButton.text = '👨🏼‍💻';
+    toggleButton.tooltip = 'Toggle Theme';
+    toggleButton.show();
+    
+
+    
+
+    // Register the toggle command
+    let disposable = vscode.commands.registerCommand('turn-the-light.switch', function () {
+        // Get the current theme
+        let currentTheme = vscode.workspace.getConfiguration().get('workbench.colorTheme');
+
+        // Determine the new theme
+        let newTheme = currentTheme === 'Default Light+' ? defaultTheme : 'Default Light+';
+
+        // Update the configuration
+        vscode.workspace.getConfiguration().update('workbench.colorTheme', newTheme, true).then(() => {
+            // Update the toggle button text
+            toggleButton.text = newTheme === 'Default Light+' ? '🔅' : '👨🏼‍💻';
+
+            // Show a message
+            vscode.window.showInformationMessage(`Theme changed to "${newTheme}"`);
+        });
+    });
+
+    // Add the disposable to the context
+    context.subscriptions.push(disposable);
+}
+
+// Deactivate the extension
+function deactivate() {
+    vscode.workspace.getConfiguration().update('workbench.colorTheme', defaultTheme, true);
+    
+}
+
+
+module.exports = {
+	activate,
+	deactivate
+}
