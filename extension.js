f50668b293c34de1a9166145908815e1865c2836
Gabriele Fontana <gafreax@gmail.com>
2023-02-01 03:57:26 +0100

    load item at start

--- extension.js
+++ extension.js
@@ -1,47 +1,63 @@
 const vscode = require("vscode");
-let defaultTheme = vscode.workspace.getConfiguration().get('workbench.colorTheme');
+let defaultTheme = vscode.workspace
+  .getConfiguration()
+  .get("workbench.colorTheme");
 
 // Activate the extension
 function activate(context) {
-    // Create the toggle button
-    let toggleButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10000000);
-    toggleButton.command = 'turn-the-light.switch';
-    toggleButton.text = '👨🏼‍💻';
-    toggleButton.tooltip = 'Toggle Theme';
-    toggleButton.show();
-    
-    context.subscriptions.push(toggleButton);
+  // Create the toggle button
+  let toggleButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    10000000
+  );
+  toggleButton.command = "turn-the-light.switch";
+  toggleButton.text = "👨🏼‍💻";
+  toggleButton.tooltip = "Toggle Theme";
+  toggleButton.show();
 
-    // Register the toggle command
-    let disposable = vscode.commands.registerCommand('turn-the-light.switch', function () {
-        // Get the current theme
-        let currentTheme = vscode.workspace.getConfiguration().get('workbench.colorTheme');
+  context.subscriptions.push(toggleButton);
 
-        // Determine the new theme
-        let newTheme = currentTheme === 'Default Light+' ? defaultTheme : 'Default Light+';
+  // Register the toggle command
+  let disposable = vscode.commands.registerCommand(
+    "turn-the-light.switch",
+    function () {
+      // Get the current theme
+      let currentTheme = vscode.workspace
+        .getConfiguration()
+        .get("workbench.colorTheme");
 
-        // Update the configuration
-        vscode.workspace.getConfiguration().update('workbench.colorTheme', newTheme, true).then(() => {
-            // Update the toggle button text
-            toggleButton.text = newTheme === 'Default Light+' ? '🔅' : '👨🏼‍💻';
+      // Determine the new theme
+      let newTheme =
+        currentTheme === "Default Light+" ? defaultTheme : "Default Light+";
 
-            // Show a message
-            vscode.window.showInformationMessage(`Theme changed to "${newTheme}"`);
+      // Update the configuration
+      vscode.workspace
+        .getConfiguration()
+        .update("workbench.colorTheme", newTheme, true)
+        .then(() => {
+          // Update the toggle button text
+          toggleButton.text = newTheme === "Default Light+" ? "🔅" : "👨🏼‍💻";
+
+          // Show a message
+          vscode.window.showInformationMessage(
+            `Theme changed to "${newTheme}"`
+          );
         });
-    });
+    }
+  );
 
-    // Add the disposable to the context
-    context.subscriptions.push(disposable);
+  // Add the disposable to the context
+  context.subscriptions.push(disposable);
 }
 
 // Deactivate the extension
 function deactivate() {
-    vscode.workspace.getConfiguration().update('workbench.colorTheme', defaultTheme, true);
-    
+  vscode.workspace
+    .getConfiguration()
+    .update("workbench.colorTheme", defaultTheme, true);
 }
-
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
